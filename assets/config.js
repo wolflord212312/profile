3cbb6c59301a6793cdd7ba98db29b83dcfadf551
DjJewl <47831173+wolflord212312@users.noreply.github.com>
2023-04-09 23:51:03 -0500

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -20,6 +20,10 @@ const config = {
         { id: "project-2", image: "https://cdn.discordapp.com/icons/948409975351443456/67abccce71478a3fd4c67549bfdd4515.png?size=4096", link: "https://discord.gg/jv6qa33SBb", name: "Oynx-Reforged" },
         { id: "project-3", image: "https://cdn.discordapp.com/icons/612695539729039411/8f27fcdba13b332553e6672a0eb68ca7.png?size=4096", link: "https://discord.com/invite/magma", name: "Magma Foundation" },
         { id: "project-4", image: "https://cdn.discordapp.com/icons/203219387627470849/a_e0e262d138d2050f2386ef2859719c8e.gif?size=4096", link: "https://discord.gg/reforgedmc", name: "Reforged-Network" },
+        { id: "project-5", image: "https://cdn.discordapp.com/attachments/931268276296769536/956890750698811444/communityIcon_1m31rv5ma5o81.png", link: "https://www.jumptask.io/r/kiheradacoma", name: "JumpTask " },
+        { id: "project-6", image: "https://dashboard.honeygain.com/images/logos/logo-main.svg", link: "https://r.honeygain.me/TALON40E02", name: "Honeygain" },
+        { id: "project-7", image: "https://earnapp.com/dashboard/www/earnapp_cp/pub/img/earnapp/logo_dm.svg?md5=9158-59334b89", link: "https://earnapp.com/i/c7se45g", name: "EarnApp" },
+        { id: "project-8", image: "https://brightvpn.com/wp-content/uploads/2021/07/logo.png", link: "https://brightvpn.com/?r=c7se45g", name: "BightVPN" },
     ],
 
     SOCIALS: [
